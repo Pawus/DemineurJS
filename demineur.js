a91fd47c3bfe922aa590e407e5edcdb436cf881a
Nicolas Lidon <pawus13@gmail.com>
2016-11-25 15:48:34 +0100

    Amorce du projet

---
+++ demineur.js
@@ -0,0 +1,90 @@
+function Demineur(length, weigth){
+	this.length = length
+	this.weigth = weigth
+	this.table = new Array(length)
+	for (var i = 0; i < length; i++){
+		this.table[i] = new Array(weigth)
+		for (var j = 0; j < weigth; j++){
+			if (Math.random() > 0.7)
+				this.table[i][j] = true
+			else
+				this.table[i][j] = false
+		}
+	}
+}
+
+function displayDemineur(demi){
+	var divDemi = document.getElementById("demineur")
+	var divResult = document.createElement("div")
+	divResult.setAttribute("id", "result")
+	divDemi.appendChild(divResult)
+	for (var i = 0; i<demi.length; i++){
+		var divL = document.createElement("tr")
+		divL.setAttribute('id', "tab"+i)
+		divDemi.appendChild(divL)
+		for (var j = 0; j < demi.weigth; j++){
+			var divW = document.createElement("th")
+			divW.setAttribute('id', i+"-"+j)
+			divW.append(demi.table[i][j]) // Permet d'afficher la valeur
+			var check = document.createElement("input")
+			check.setAttribute('type', 'checkbox')
+			check.setAttribute('checked', "checked")
+			check.setAttribute('id', i+"-"+j)
+			check.addEventListener('click', function(){
+				id = this.id.split("-")
+				checkMine(demi, id[0], id[1])
+				})
+			divW.appendChild(check)
+			divL.appendChild(divW)
+
+		}
+	}
+}
+
+function checkMine(demi, l, w){
+	if (demi.table[l][w]) {
+		// Tout afficher
+		var divResult = document.getElementById("result")
+		console.log(divResult)
+		divResult.append("Perdu !")
+	}
+	/*
+	9 9 9 
+	9 9 9
+	9 9 9
+	*/
+	else {
+		var cptMine = 0
+
+		console.log("Click", l, w)
+		if(l > 0 && demi.table[l-1][w]){
+			cptMine++
+			if(w > 0 && demi.table[l-1][w-1])
+				cptMine++
+		}
+		if(l < demi.length && demi.table[l- -1][w]){
+			cptMine++
+			if(w < demi.weigth && demi.table[l- -1][w- -1])
+				cptMine++
+		}
+		if(w > 0 && demi.table[l][w-1]){
+			cptMine++
+			if(l > 0 && demi.table[l-1][w-1])
+				cptMine++
+		}
+		if(w < demi.weigth && demi.table[l][w- -1]){
+			console.log('')
+			cptMine++
+			if(l < demi.length && demi.table[l- -1][w- -1])
+				cptMine++
+		}
+		
+		console.log(cptMine)
+	}
+}
+var demi = new Demineur(10, 10)
+
+displayDemineur(demi)
+
+console.log(demi)
+
